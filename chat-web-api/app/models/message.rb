4ffa7358706b8ad22da762b5bed69d71fc7c7fd6
Adam Rodrigues <adam@factrpay.io>
2021-09-16 19:57:22 -0400

    Return user in json on message return

--- chat-web-api/app/models/message.rb
+++ chat-web-api/app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :user
+  belongs_to :channel
   
+  def as_json(options={})
+      {
+        id: id,
+        message: message,
+        timeStamp: created_at,
+        channelId: channel_id,
+        user: {
+          id: user.id,
+          name: user.name
+        }
+      }
+  end
 end
