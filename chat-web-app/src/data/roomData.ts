9643542e126f77013f2fda6ba1bae2b9b8cbe583
Adam Rodrigues <adam@factrpay.io>
2021-09-16 12:29:09 -0400

    Mock data for front-end

--- chat-web-app/src/data/roomData.ts
+++ chat-web-app/src/data/roomData.ts
@@ -1,3 +1,13 @@
 import { ChatRoom } from "../types/ChatRoom"
 
-export const rooms: ChatRoom[] = []
+//Mock data while preparing the front-end
+export const rooms: ChatRoom[] = [
+  {
+    id: "32323",
+    name: "Homestars Room",
+  },
+  {
+    id: "34343",
+    name: "Adam Rodrigue's Room",
+  },
+]
