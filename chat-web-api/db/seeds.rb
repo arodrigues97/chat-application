4341f9b537440596057bbadac4faf663553909ea
Adam Rodrigues <adam@factrpay.io>
2021-09-17 09:51:59 -0400

    Implement database seeding

--- chat-web-api/db/seeds.rb
+++ chat-web-api/db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+ User.create(
+    {
+        id: 1,
+        name: "Adam Rodrigues",
+    },)
+    User.create( {
+        id: 2,
+        name: "Ankit Kanojia"
+    })
+
+    Channel.create(
+        {
+            name: "Adam's Channel",
+            user_id: 1
+        }.
+
+Channel.create({name: "Ankit's Channel", user_id: 2})
+
+
+ChannelJoined.create({user_id: 1, channel_id: 1})
+ChannelJoined.create({user_id: 2, channel_id: 2})
+
+Message.create(
+    {
+        message: "Hello, It's me Adam",
+        channel_id: 1,
+        user_id: 1
+    }
+)
+
+Message.create(
+    {
+        message: "Hello, It's me Ankit",
+        channel_id: 2,
+        user_id: 2
+    }
+)
