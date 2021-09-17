c17a8640c9682caa5d1288e07b4306461679a4d9
Adam Rodrigues <adam@factrpay.io>
2021-09-17 09:59:17 -0400

    Add database seeding

--- chat-web-api/db/seeds.rb
+++ chat-web-api/db/seeds.rb
@@ -3,44 +3,20 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([ name: 'Star Wars' ,  name: 'Lord of the Rings' ])
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
- User.create(
-    {
-        id: 1,
-        name: "Adam Rodrigues",
-    },)
-    User.create( {
-        id: 2,
-        name: "Ankit Kanojia"
-    })
+ User.create!(id: 1, name: "Adam Rodrigues")
+ User.create!(id: 2, name: "Ankit Kanojia")
 
-    Channel.create(
-        {
-            name: "Adam's Channel",
-            user_id: 1
-        }.
-
-Channel.create({name: "Ankit's Channel", user_id: 2})
+Channel.create!(id: 1, name: "Adam's Channel", user_id: 1)
+Channel.create!(id: 2, name: "Ankit's Channel", user_id: 2)
 
 
-ChannelJoined.create({user_id: 1, channel_id: 1})
-ChannelJoined.create({user_id: 2, channel_id: 2})
+ChannelJoined.create!(user_id: 1, channel_id: 1)
+ChannelJoined.create!(user_id: 2, channel_id: 2)
 
-Message.create(
-    {
-        message: "Hello, It's me Adam",
-        channel_id: 1,
-        user_id: 1
-    }
-)
+Message.create!( message: "Hello, It's me Adam",  channel_id: 1, user_id: 1)
 
-Message.create(
-    {
-        message: "Hello, It's me Ankit",
-        channel_id: 2,
-        user_id: 2
-    }
-)
+Message.create!(  message: "Hello, It's me Ankit", channel_id: 2,  user_id: 2)
